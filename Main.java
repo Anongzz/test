969ece978ea5dd7b14a8c8c9e253c230e9903cef
안효웅 <skow951216@gmail.com>
2023-02-26 15:54:40 +0900

    test git intellij

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+
+        int a = 1;
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("zz");
+
+        }
+
+
+
+        System.out.println(a);
+
+    }
+
+}
